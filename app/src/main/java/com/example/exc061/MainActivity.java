911ad3ac48608f3116b6d81f1ddb02d795b81dc6
dz2244 <dz2244@bs.amalnet.k12.il>
2023-12-23 16:34:40 +0200

    name2

--- app/src/main/java/com/example/exc061/MainActivity.java
+++ app/src/main/java/com/example/exc061/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.exc061;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn ;
+    TextView text ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        text = findViewById(R.id.text);
+    }
+
+    public void ace(View view) {
+        text.setText("Oh, yea, I’ve been clicked!");
+        text.setTextColor(Color.CYAN);
+
     }
 }
